d89b3e8fb6ec2154d9ba9ab8e0611a69d7d2cb3e
GSansigolo <gabrielsansigolo@gmail.com>
2019-06-24 13:14:29 -0300

    Add Pages and Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RepositoriesComponent } from './pages/repositories/repositories.component';
+import { DatasetsComponent } from './pages/datasets/datasets.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'repositories', component: RepositoriesComponent },
+  { path: 'datasets', component: DatasetsComponent },
+  { path: 'login', component: LoginComponent },
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
+import { HomeComponent } from './pages/home/home.component';
+import { RepositoriesComponent } from './pages/repositories/repositories.component';
+import { DatasetsComponent } from './pages/datasets/datasets.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RepositoriesComponent,
+    DatasetsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
