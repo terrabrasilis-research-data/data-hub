0c49ba30b04e32e02eff7d30920beb778d219d78
GSansigolo <gabrielsansigolo@gmail.com>
2019-06-25 13:59:09 -0300

    Fix Header

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
+  declarations: [LayoutComponent, FooterComponent],
   exports: [LayoutComponent],
   imports: [
     CommonModule
