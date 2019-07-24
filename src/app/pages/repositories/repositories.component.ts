c4e1a40cbb195cd5a7d318d7d12f4019b33cb587
sansigolo <gabrielsansigolo@gmail.com>
2019-07-24 15:54:04 -0300

    Fix Repo table

--- src/app/pages/repositories/repositories.component.ts
+++ src/app/pages/repositories/repositories.component.ts
@@ -11,7 +11,7 @@ import {MatPaginator, MatTableDataSource} from '@angular/material';
 
 export class RepositoriesComponent implements OnInit {
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = ['name', 'image', 'abstract'];
   dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
@@ -73,30 +73,29 @@ export class RepositoriesComponent implements OnInit {
 
 export interface Element {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  image: string;
+  abstract: string;
 }
 
 const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  {name: 'Hydrogen Repository', image: "Kuhn, Gerhard & Johnsen, Sigfus J", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Helium Repository', image: " Hillenbrand, Claus-Dieter", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Lithium Repository', image: "Kuhn, Gerhard & Oerter, Hans", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Beryllium Repository', image: "Kuhn, Gerhard", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Boron Repository', image: "Oerter, Hans", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Carbon Repository', image: "Kuhn, Gerhard & Hillenbrand, Claus-Dieter", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Nitrogen Repository', image: "Oerter, Hans", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Oxygen Repository', image: "Kuhn, Gerhard", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Fluorine Repository', image: "Oerter, Hans", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Neon Repository', image: "Kuhn, Gerhard & Oerter, Hans", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Sodium Repository', image: "Johnsen, Sigfus J", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Magnesium Repository', image: "Hillenbrand, Claus-Dieter", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Aluminum Repository', image: "Johnsen, Sigfus J", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Silicon Repository', image: "Kuhn, Gerhard & Johnsen, Sigfus J", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Phosphorus Repository', image: " Hillenbrand, Claus-Dieter", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Sulfur Repository', image: "Johnsen, Sigfus J", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Chlorine Repository', image: "Kuhn, Gerhard & Hillenbrand, Claus-Dieter", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Argon Repository', image: "Hillenbrand, Claus-Dieter & Oerter, Hans", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Potassium Repository', image: "Oerter, Hans & Johnsen, Sigfus J", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
+  {name: 'Calcium Repository', image: "Kuhn, Gerhard & Hillenbrand, Claus-Dieter", abstract: "Some quick example text to build on the card title and make up the bulk of the card's content."},
 ];
