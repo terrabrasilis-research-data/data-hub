37c71c4228750715c579b88275e1a901a59a850e
GSansigolo <gabrielsansigolo@gmail.com>
2019-08-07 14:44:57 -0300

    Finish Login ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 
+let logged = false;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'terrabrasilis-research-data';
-
+  user = {'name':"Gabriel"};
   OpenMenu() {
     if (document.getElementById("wrapper").className == "d-flex")
       document.getElementById("wrapper").className = "d-flex toggled";
     else  
       document.getElementById("wrapper").className = "d-flex";
+  }
+
+  ChangeLogged(results: boolean){
+    logged = results;
+  }
+
+  isLogged(){
+    return logged;
   }
 }
