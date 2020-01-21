a115f9f3b53b7939045c0ff80bdff4d7f9b0af57
GSansigolo <gabrielsansigolo@gmail.com>
2020-01-21 14:04:39 -0300

    wip ckan user_create

--- src/app/pages/signup/signup.service.ts
+++ src/app/pages/signup/signup.service.ts
@@ -9,10 +9,10 @@ export class SignupService {
   }
 
   public async user_create(name: string, email: string, password: string, full_name: string): Promise<any> {
-    const response = await this.http.post(`http://localhost:5000/api/3/action/user_create`, {'name': name, 'email': email, 'password': password, 'fullname': full_name},{
-      headers: {
+    const response = await this.http.post(`http://localhost:5000/api/3/action/user_create`, {'name': name, 'email': email, 'password': password, 'fullname': full_name}, {
+      headers: new HttpHeaders ({
         Authorization: '0498a5c2-24a5-4848-a23b-29044879c1fa'
-      }
+      })
     }).toPromise();
   }
 
