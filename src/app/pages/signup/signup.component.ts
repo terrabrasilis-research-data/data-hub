a95388221315b5d4f511ead6dd50929c7b332a65
GSansigolo <gabrielsansigolo@gmail.com>
2020-01-28 12:22:01 -0300

    close #17 and close #24

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -72,25 +72,32 @@ export class SignupComponent implements OnInit {
     });
   }
 
-  private async onSubmit() {
+private async onSubmit() {
     try {
-      const response = await this.ss.user_create(this.username, this.email, this.password, this.fullname);
-      const response2 = await this.ss.user_create_db(this.username, this.email, this.fullname, '2019-09-04T14:48:54+00:00', '2019-09-04T14:48:54+00:00');
-
-      if (response2) {
-        console.log(response)
-        console.log(response2)
-        this.formGroup.reset(); 
-        this.showMsg= true;
-      } 
+        const response = await this.ss.user_create(this.username, this.email, this.password, this.fullname);
+        if (response) {
+            try {
+                console.log(response)
+                const response2 = await this.ss.user_create_db(this.username, this.password, this.email, this.fullname, '2019-09-04T14:48:54+00:00', '2019-09-04T14:48:54+00:00', response['result'].apikey);
+                if (response2) {
+                    console.log(response2)
+                    this.formGroup.reset();
+                    this.showMsg = true;
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        }
     } catch (err) {
-      console.log(err)
-    } 
-  }
+        console.log(err)
+    }
+}
 
   onReset() {
     this.formGroup.reset();
   }
   
 }
+
+
 
