ff6204955a38d282479b1e35ef97b39f1fa299b3
GSansigolo <gabrielsansigolo@gmail.com>
2020-02-18 11:19:28 -0300

    fix groups name and update group page

--- src/app/pages/signup/signup.service.ts
+++ src/app/pages/signup/signup.service.ts
@@ -11,7 +11,7 @@ export class SignupService {
   public async user_create(name: string, email: string, password: string, full_name: string): Promise<any> {
     const response = await this.http.post(`http://localhost:5000/api/3/action/user_create`, {'name': name, 'email': email, 'password': password, 'fullname': full_name}, {
       headers: new HttpHeaders ({
-        Authorization: '12b7549f-50c6-479d-b274-281649f02c36'
+        Authorization: '91303f75-738b-4df8-9dcd-608c92c00875'
       })
     }).toPromise();
     return response;
