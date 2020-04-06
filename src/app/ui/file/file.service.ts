b131f3964f85c87a0421eec3d8230ab41ea8a508
GSansigolo <gabrielsansigolo@gmail.com>
2020-04-06 13:47:49 -0300

    Fix Security Alerts

--- src/app/ui/file/file.service.ts
+++ src/app/ui/file/file.service.ts
@@ -8,8 +8,10 @@ export class FileService {
   constructor(private http: HttpClient) {}
 
   public async uploadFile(file: File, repo_id: number): Promise<any> {
+
     const formData = new FormData();
     formData.append('file', file, file.name);
+
     const response = await this.http.post(`http://localhost:8090/api/v1.0/file_upload/`+repo_id, formData).toPromise();
 
     return response[0]['data_url']
