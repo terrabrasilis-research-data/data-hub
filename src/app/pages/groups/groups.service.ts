f3723df0571758c2dc253f41a8f580039e661b10
GSansigolo <gabrielsansigolo@gmail.com>
2020-06-02 17:19:21 -0300

    update services port

--- src/app/pages/groups/groups.service.ts
+++ src/app/pages/groups/groups.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CKAN_User } from '../newgroup/newgroup.component';
+import { SignupService } from '../signup/signup.service';
 
 @Injectable({ providedIn: 'root' })
 export class GroupsService {
@@ -9,33 +10,36 @@ export class GroupsService {
   constructor(private http: HttpClient) {
   }
 
+  CKAN_PORT = '5000';
+  TBRD_API_PORT = '8090';
+  
   public async get_groups(): Promise<any> {
-    const response = await this.http.get(`http://127.0.0.1:8090/api/v1.0/groups`).toPromise();
+    const response = await this.http.get(`http://127.0.0.1:`+this.TBRD_API_PORT+`/api/v1.0/groups`).toPromise();
     return response['groups'];
   }
 
   public async get_groups_from_user(user_id: number): Promise<any> {
-    const response = await this.http.get(`http://127.0.0.1:8090/api/v1.0/groups_from_user/`+ user_id).toPromise();
+    const response = await this.http.get(`http://127.0.0.1:`+this.TBRD_API_PORT+`/api/v1.0/groups_from_user/`+ user_id).toPromise();
     return response['groups'];
   }
 
   public async get_categories(): Promise<any> {
-    const response = await this.http.get(`http://127.0.0.1:8090/api/v1.0/categories`).toPromise();
+    const response = await this.http.get(`http://127.0.0.1:`+this.TBRD_API_PORT+`/api/v1.0/categories`).toPromise();
     return response;
   }
 
   public async get_users(): Promise<any> {
-    const response = await this.http.get(`http://localhost:5001/api/3/action/user_list`).toPromise();
+    const response = await this.http.get(`http://localhost:`+this.CKAN_PORT+`/api/3/action/user_list`).toPromise();
     return response;
   }
 
   public async get_users_db(): Promise<any> {
-    const response = await this.http.get(`http://localhost:8090/api/v1.0/users`).toPromise();
+    const response = await this.http.get(`http://localhost:`+this.TBRD_API_PORT+`/api/v1.0/users`).toPromise();
     return response;
   }
 
   public async get_user(user_id: number): Promise<any> {
-    const response = await this.http.get(`http://localhost:8090/api/v1.0/users/`+user_id).toPromise();
+    const response = await this.http.get(`http://localhost:`+this.TBRD_API_PORT+`/api/v1.0/users/`+user_id).toPromise();
     return response;
   }
 
@@ -44,7 +48,7 @@ export class GroupsService {
     /*
     CREATE GROUP CKAN
     */
-    const responseGroupCkan = await this.http.post(`http://localhost:5001/api/3/action/group_create`, {'name': groupurl, 'title': name, 'description': description, 'state': 'active', 'users': users }, {
+    const responseGroupCkan = await this.http.post(`http://localhost:`+this.CKAN_PORT+`/api/3/action/group_create`, {'name': groupurl, 'title': name, 'description': description, 'state': 'active', 'users': users }, {
       headers: new HttpHeaders ({
         Authorization: ckan_api_key
       })
@@ -55,7 +59,7 @@ export class GroupsService {
     /*
     CREATE GROUP
     */
-    const reponseGroup = await this.http.post(`http://127.0.0.1:8090/api/v1.0/groups`, {'name': name, 'abstract': description,  'maintainer': maintainer, 'created_on': created_on, 'language': language, 'ckan_group_id': ckan_group_id, 'image': 'http://localhost:8090/api/v1.0/uploads/'+image}, {
+    const reponseGroup = await this.http.post(`http://127.0.0.1:`+this.TBRD_API_PORT+`/api/v1.0/groups`, {'name': name, 'abstract': description,  'maintainer': maintainer, 'created_on': created_on, 'language': language, 'ckan_group_id': ckan_group_id, 'image': 'http://localhost:`+this.TBRD_API_PORT+`/api/v1.0/uploads/'+image}, {
       headers: new HttpHeaders ({
           Authorization: 'Bearer ' + userToken
       })
@@ -68,7 +72,7 @@ export class GroupsService {
       /*
       CREATE USER_GROUP_REL
       */
-      const responseUserGroup = await this.http.post(`http://127.0.0.1:8090/api/v1.0/user_group_rel`, {'user_id': users[index].user_id, 'group_id': group_id}, {
+      const responseUserGroup = await this.http.post(`http://127.0.0.1:`+this.TBRD_API_PORT+`/api/v1.0/user_group_rel`, {'user_id': users[index].user_id, 'group_id': group_id}, {
         headers: new HttpHeaders ({
             Authorization: 'Bearer ' + userToken
         })
--- src/app/pages/signup/signup.service.ts
+++ src/app/pages/signup/signup.service.ts
@@ -3,32 +3,37 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class SignupService {
+
+  static CKAN_PORT: string;
+  static TBRD_API_PORT: string;
   
   /** start http service client */
   constructor(private http: HttpClient) {
   
   }
   
-  public static API_KEY= '4875ee2a-c90c-4a9c-8f61-342e2a9171e7';
- 
+  API_KEY = '4875ee2a-c90c-4a9c-8f61-342e2a9171e7';
+  CKAN_PORT = '5000';
+  TBRD_API_PORT = '8090';
+
   public async user_create(name: string, email: string, password: string, full_name: string, time: string): Promise<any> {
     
-    const response = await this.http.post(`http://localhost:5001/api/3/action/user_create`, {'name': name, 'email': email, 'password': password, 'fullname': full_name}, {
+    const response = await this.http.post(`http://localhost:`+this.CKAN_PORT+`/api/3/action/user_create`, {'name': name, 'email': email, 'password': password, 'fullname': full_name}, {
       headers: new HttpHeaders ({
-        Authorization: SignupService.API_KEY
+        Authorization: this.API_KEY
       })
     }).toPromise();
 
     let apikey = response['result'].apikey
 
-    const response2 = await this.http.post(`http://localhost:8090/api/v1.0/users`, {'username': name, 'password': password, 'email': email, 'image': 'assets/images/img_avatar.png', 'created_on': time, 'last_login': time, 'full_name': full_name, 'ckan_api_key': apikey}).toPromise();
+    const response2 = await this.http.post(`http://localhost:`+this.TBRD_API_PORT+`/api/v1.0/users`, {'username': name, 'password': password, 'email': email, 'image': 'assets/images/img_avatar.png', 'created_on': time, 'last_login': time, 'full_name': full_name, 'ckan_api_key': apikey}).toPromise();
     
     return response2;
   }
 
   public async user_update(userToken: string, user_id: number, username: string, email: string, password: string, full_name: string, created_on: string, last_login:string, image: string, apikey: string): Promise<any> {
     
-    const response = await this.http.put(`http://localhost:8090/api/v1.0/users/`+user_id, { 'username': username, 'password': password, 'email': email, 'image': 'http://localhost:8090/api/v1.0/uploads/'+image, 'created_on': created_on, 'last_login': last_login, 'full_name': full_name, 'ckan_api_key': apikey }, {
+    const response = await this.http.put(`http://localhost:`+this.TBRD_API_PORT+`/api/v1.0/users/`+user_id, { 'username': username, 'password': password, 'email': email, 'image': 'http://localhost:`+this.TBRD_API_PORT+`/api/v1.0/uploads/'+image, 'created_on': created_on, 'last_login': last_login, 'full_name': full_name, 'ckan_api_key': apikey }, {
       headers: new HttpHeaders ({
           Authorization: 'Bearer ' + userToken
       })
@@ -38,34 +43,34 @@ export class SignupService {
   }
 
   public async get_members(id: string): Promise<any> {
-    const response = await this.http.post(`http://localhost:5001/api/3/action/member_list`, {'id': id, 'object_type': 'user'}, {
+    const response = await this.http.post(`http://localhost:`+this.CKAN_PORT+`/api/3/action/member_list`, {'id': id, 'object_type': 'user'}, {
       headers: new HttpHeaders ({
-        Authorization: SignupService.API_KEY
+        Authorization: this.API_KEY
       })
     }).toPromise();
     return response;
   }
 
   public async get_users_ckan(): Promise<any> {
-    const response = await this.http.get(`http://localhost:5001/api/3/action/user_list`, {
+    const response = await this.http.get(`http://localhost:`+this.CKAN_PORT+`/api/3/action/user_list`, {
       headers: new HttpHeaders ({
-        Authorization: SignupService.API_KEY
+        Authorization: this.API_KEY
       })
     }).toPromise();
     return response;
   }
   
   public async organization_show(id: string): Promise<any> {
-    const response = await this.http.post(`http://localhost:5001/api/3/action/organization_show`, {'id': id}, {
+    const response = await this.http.post(`http://localhost:`+this.CKAN_PORT+`/api/3/action/organization_show`, {'id': id}, {
       headers: new HttpHeaders ({
-        Authorization: SignupService.API_KEY
+        Authorization: this.API_KEY
       })
     }).toPromise();
     return response;
   }
 
   public async get_activity(ckan_api_key: string): Promise<any> {
-    const response = await this.http.post(`http://localhost:5001/api/3/action/dashboard_activity_list`, {'limit': 6}, {
+    const response = await this.http.post(`http://localhost:`+this.CKAN_PORT+`/api/3/action/dashboard_activity_list`, {'limit': 6}, {
       headers: new HttpHeaders ({
         Authorization: ckan_api_key
       })
